7163e8fc9b04042c95873fa1e834884d8d824c8c
Sv <example@gmail.com>
2020-06-01 13:55:08 +0300

    class document & object

--- WebApplication5/Models/Document.cs
+++ WebApplication5/Models/Document.cs
@@ -7,5 +7,9 @@ namespace WebApplication5.Models
 {
     public class Document
     {
+        //id документа
+        public string Id { get; set; }
+        //Наименование документа
+        public string Name { get; set; }
     }
 }
--- WebApplication5/Models/Object.cs
+++ WebApplication5/Models/Object.cs
@@ -7,5 +7,9 @@ namespace WebApplication5.Models
 {
     public class Object
     {
+        //id объекта
+        public string Id { get; set; }
+        //Наименование объекта
+        public string Name { get; set; }
     }
 }
